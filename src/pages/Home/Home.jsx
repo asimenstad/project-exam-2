6ac2461b06b8a68e518bce42f985c5d538c6333b
asimenstad <a-nna-s@hotmail.com>
2023-05-16 12:22:16 +0200

    Fixed hero

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,14 +12,8 @@ function Home() {
     setSearchInput(e.target.value);
   }
 
-  const heroImage = [
-    "https://images.unsplash.com/photo-1618064541372-289bdb6f5b7b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2233&q=80",
-    "https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1794&q=80",
-    "https://images.unsplash.com/photo-1504512485720-7d83a16ee930?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1808&q=80",
-    "https://images.unsplash.com/photo-1580137189272-c9379f8864fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
-  ];
-
-  const getIndex = Math.floor(Math.random() * heroImage.length);
+  const heroImage =
+    "https://images.unsplash.com/photo-1618064541372-289bdb6f5b7b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2233&q=80";
 
   return (
     <Container component="main">
@@ -41,7 +35,7 @@ function Home() {
       <Box
         sx={{
           height: 350,
-          backgroundImage: `url(${heroImage[getIndex]})`,
+          backgroundImage: `url(${heroImage})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
