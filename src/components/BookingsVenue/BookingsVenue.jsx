14f3095e81dd77ea5bf981cb16dba55bf4a48d3f
asimenstad <a-nna-s@hotmail.com>
2023-05-22 10:45:33 +0200

    Added favicon

--- src/components/BookingsVenue/BookingsVenue.jsx
+++ src/components/BookingsVenue/BookingsVenue.jsx
@@ -3,10 +3,9 @@ import { Box, Divider, Typography } from "@mui/material";
 import { format } from "date-fns";
 
 function BookingsVenue({ bookings }) {
-  console.log(bookings);
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-      <Typography variant="h2">Bookings</Typography>
+      <Typography variant="h2">Bookings ({bookings.length})</Typography>
       {bookings.map(({ created, dateFrom, dateTo, guests }) => (
         <Box sx={{ bgcolor: "#fff", display: "flex", flexDirection: "column", p: 3, borderRadius: 1 }}>
           <Typography variant="overline">{format(new Date(created), "PP")}</Typography>
